4902ee1a012754550c71b53a25a1807cfb38a98d
Nestor Perez <Nestor.Perez@cencosudscotiabank.cl>
2021-12-16 13:26:48 -0500

    first commit

---
+++ src/main/java/com/nisum/tech/NisumApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumApplication.class, args);
+	}
+
+}
